a993cc81fc6f238fb3e0d38255a22a47a6796035
Juatson <juanestebanortiz2@gmail.com.co>
2023-10-30 19:03:41 -0500

    first commit

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button Registrase, Iniciarsesion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Registrase=findViewById(R.id.Registrase);
+        Iniciarsesion=findViewById(R.id.Iniciarsesion);
+
+        Intent second = new Intent(getApplicationContext(),com.example.myapplication.pprincipal.class);
+
+        Iniciarsesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(second);
+            }
+        });
+
     }
 }
